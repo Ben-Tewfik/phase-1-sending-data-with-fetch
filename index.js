9eeee6a5269041096e8908b2f89bd185c6032465
Benarba Tewfik <tewfikhotspur@gmail.com>
2023-08-24 23:04:26 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name, email) {
+  const UserData = {
+    name: name,
+    email: email,
+  };
+  return fetch(`http://localhost:3000/users`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(UserData),
+  })
+    .then(resp => resp.json())
+    .then(data => {
+      const id = data.id;
+      const p = document.createElement("p");
+      p.textContent = `the id : ${id}`;
+      document.body.appendChild(p);
+    })
+    .catch(error => {
+      const errorMessage = error.message;
+      const p = document.createElement("p");
+      p.textContent = `error : ${errorMessage}`;
+      document.body.appendChild(p);
+    });
+}
